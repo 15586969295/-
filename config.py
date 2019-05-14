9c1ae6eb15596e61085ac2231aca98e9063a80f1
ReturnFalse <42865648+15586969295@users.noreply.github.com>
2019-05-14 22:20:50 +0800

    集成log日志

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
+import logging
+
+
 class Config(object):
     """项目的配置"""
     DEBUG = True
@@ -5,3 +8,4 @@ class Config(object):
     # 为数据库添加配置
     SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/db_rento"  # mysql://username:password@ip:port/dbname
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    LOG_LELVEL = logging.DEBUG
