71260ea18e7427cae25b6345f1522e4e7965f69c
ReturnFalse <42865648+15586969295@users.noreply.github.com>
2019-05-14 21:54:42 +0800

    创建配置，配置数据库

--- back_end/manage.py
+++ back_end/manage.py
@@ -1,6 +1,22 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+class Config(object):
+    """项目的配置"""
+    DEBUG = True
+
+    # 为数据库添加配置
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/db_rento"  # mysql://username:password@ip:port/dbname
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 app = Flask(__name__)
+# 加载配置
+app.config.from_object(Config)
+
+# 初始化数据库
+db = SQLAlchemy(app)
 
 
 @app.route('/')
